73f2866d0806386ce75bf250b1e749b2a4494733
Adeesha <adeesha@ecyber.com>
2022-07-02 20:31:37 +0530

    Sorting file content using lamda expression. Showing lengthy rows at first and others later

---
+++ src/Task5/File5.java
@@ -0,0 +1,26 @@
+package Task5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class File5 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader r = new BufferedReader(new FileReader("data/wolf-fox.txt"));
+		
+		r.lines()
+		.sorted((a,b)-> {
+			if(a.length()==b.length())
+				return 0;
+			if(a.length()<b.length())
+				return 1;
+			return -1;
+		})
+		.forEach(l-> System.out.println(l));	
+		
+		r.close();
+	}
+
+}
