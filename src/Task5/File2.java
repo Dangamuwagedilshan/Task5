ba5e5fb10bd7f06407b71a2ae35f6230280ecccc
Adeesha <adeesha@ecyber.com>
2022-07-02 20:07:55 +0530

    count the lines of the file

---
+++ src/Task5/File2.java
@@ -0,0 +1,17 @@
+package Task5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class File2 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader r = new BufferedReader(new FileReader("data/wolf-fox.txt"));
+		//r.lines().forEach(l-> System.out.println(l));
+		System.out.println("Line count of the file: "+r.lines().count());
+		r.close();
+	}
+
+}
