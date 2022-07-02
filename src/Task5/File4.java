62cbb135350387823bd6abaf8f30f56400137b5e
Adeesha <adeesha@ecyber.com>
2022-07-02 20:20:02 +0530

    Map the file content to upper case

---
+++ src/Task5/File4.java
@@ -0,0 +1,20 @@
+package Task5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class File4 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader r = new BufferedReader(new FileReader("data/wolf-fox.txt"));
+		
+		r.lines()
+		.map(l-> l.toUpperCase())
+		.forEach(l-> System.out.println(l));	
+		
+		r.close();
+	}
+
+}
