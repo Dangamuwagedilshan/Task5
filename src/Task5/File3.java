690fa9b6a91de393fa65227ab1a435755c2234d9
Adeesha <adeesha@ecyber.com>
2022-07-02 20:15:51 +0530

    Filter the "his" in the file and print it out

---
+++ src/Task5/File3.java
@@ -0,0 +1,18 @@
+package Task5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class File3 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader r = new BufferedReader(new FileReader("data/wolf-fox.txt"));
+		
+		r.lines().filter(l->l.contains("his")).forEach(l-> System.out.println(l));	
+		
+		r.close();
+	}
+
+}
