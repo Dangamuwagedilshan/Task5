a507911ac7f8d1c7d79e50d86db400976d8f01ff
Adeesha <adeesha@ecyber.com>
2022-07-03 18:35:39 +0530

    Concat and print content of the file in a one line

---
+++ src/fileHandling/File7.java
@@ -0,0 +1,25 @@
+package fileHandling;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Optional;
+
+public class File7 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader r = new BufferedReader(new FileReader("data/wolf-fox.txt"));
+		Optional<String> result = r.lines().reduce((left,right)->left.concat(" ".concat(right)));
+		
+		if(result.isPresent()) {
+			System.out.println("The result is "+result.get());
+		}else {
+			System.out.println("Result is not present");
+		}
+		
+		r.close();
+		
+	}
+
+}
