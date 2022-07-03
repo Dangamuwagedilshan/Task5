5b9613cd19e409b60e801e1e7af35ec1306d8afd
Adeesha <adeesha@ecyber.com>
2022-07-03 14:04:26 +0530

    Changed array to own array

--- src/experiments/Rugby03.java
+++ src/experiments/Rugby03.java
@@ -6,28 +6,28 @@ import java.util.OptionalInt;
 
 public class Rugby03 {
   public static void main(String[] args) {
-    List<Club> table = Arrays.asList(
-        new Club(1, "Northampton Saints", 22, 16, 1, 5, 621, 400, 221, 75, 41,
-            8, 2, 76),
-        new Club(2, "Bath", 22, 16, 0, 6, 625, 414, 211, 72, 43, 9, 2, 75),
-        new Club(3, "Leicester Tigers", 22, 15, 1, 6, 453, 421, 32, 37, 39, 4,
-            2, 68),
-        new Club(4, "Saracens", 22, 14, 1, 7, 664, 418, 246, 70, 40, 5, 5, 68),
-        new Club(5, "Exeter Chiefs", 22, 14, 0, 8, 663, 437, 226, 70, 46, 5, 7,
-            68),
-        new Club(6, "Wasps", 22, 11, 2, 9, 672, 527, 145, 77, 54, 9, 4, 61),
-        new Club(7, "Sale Sharks", 22, 11, 0, 11, 497, 482, 15, 62, 54, 6, 4,
-            54),
-        new Club(8, "Harlequins", 22, 10, 0, 12, 444, 514, -70, 45, 50, 4, 5,
-            49),
-        new Club(9, "Gloucester", 22, 9, 1, 12, 553, 575, -22, 53, 61, 4, 6,
-            48),
-        new Club(10, "London Irish", 22, 7, 1, 14, 442, 578, -136, 46, 57, 4, 6,
-            40),
-        new Club(11, "Newcastle Falcons", 22, 5, 1, 16, 475, 545, -70, 57, 61,
-            4, 8, 34),
-        new Club(12, "London Welsh", 22, 0, 0, 22, 223, 1021, -798, 29, 147, 1,
-            0, 1));
+	  List<Club> table = Arrays.asList(
+		        new Club(1, "Belfast Saints", 10, 2, 1, 15, 628, 200, 251, 85, 11,
+		            81, 24, 56),
+		        new Club(2, "Tiger", 26, 56, 0, 16, 425, 454, 111, 12, 53, 91, 20, 15),
+		        new Club(3, "wales", 221, 16, 11, 16, 443, 251, 12, 27, 31, 40,
+		            2, 68),
+		        new Club(4, "Dukes", 221, 114, 21, 47, 554, 688, 746, 100, 30, 45, 8, 28),
+		        new Club(5, "Exeter bricks", 12, 1, 7, 0, 223, 237, 96, 40, 45, 0, 71,
+		            68),
+		        new Club(6, "Hornets", 22, 71, 12, 92, 82, 127, 665, 47, 24, 1, 0, 11),
+		        new Club(7, "North sea Sharks", 22, 121, 10, 101, 327, 662, 45, 32, 24, 56, 14,
+		            54),
+		        new Club(8, "Suicide Squad", 22, 103, 0, 142, 784, 104, -40, 85, 40, 2, 5,
+		            69),
+		        new Club(9, "Berkshire", 22, 4, 5, 36, 253, 255, -46, 753, 31, 2, 0,
+		            48),
+		        new Club(10, "Norfolk Irish", 22, 40, -1, 74, 44, 78, -106, 4, 53, 44, 06,
+		            -42),
+		        new Club(11, "Middlesex Wolfs", 22, 31, -1, 76, 235, 665, -40, 507, 61,
+		            44, 80, 314),
+		        new Club(12, "East Sussex", 22, 0, -10, 29, 23, 581, -368, 259, 107, 1,
+		            0, -21));
 
 
     OptionalInt min = table.stream().mapToInt(Club::getPoints).min();
