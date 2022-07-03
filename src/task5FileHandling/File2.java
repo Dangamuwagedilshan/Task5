9be9ca7697ea209ef434bcaabfeae1b091bb0e44
Adeesha <adeesha@ecyber.com>
2022-07-03 19:30:32 +0530

    get the line count of the file and print it out

---
+++ src/task5FileHandling/File2.java
@@ -0,0 +1,17 @@
+package task5FileHandling;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class File2 {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BufferedReader r = new BufferedReader(new FileReader("data/Task5.txt"));
+		//r.lines().forEach(l-> System.out.println(l));
+		System.out.println("Line count of the file: "+r.lines().count());
+		r.close();
+	}
+
+}
